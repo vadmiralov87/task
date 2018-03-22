5a4743c81a797668fcfa8794ed735f6e3c9dce12
vadmiralov87 <vadmiralov.87@gmail.com>
2018-03-22 22:03:53 +0200

    disable new creation of instance

--- deploy.groovy
+++ deploy.groovy
@@ -3,7 +3,7 @@
 node {
 	stage('Deploy') {
 		sh '''
-           	docker-machine create --driver amazonec2 --amazonec2-region eu-west-1 --amazonec2-security-group launch-wizard-55 --amazonec2-instance-type t2.micro vadmiralov.2
+           	#docker-machine create --driver amazonec2 --amazonec2-region eu-west-1 --amazonec2-security-group launch-wizard-55 --amazonec2-instance-type t2.micro vadmiralov.2
            	eval $(docker-machine env vadmiralov.2)
            	docker run -d -p 80:80 vadmiralov87/nginx
            	echo "http://"`docker-machine ip vadmiralov.2`
