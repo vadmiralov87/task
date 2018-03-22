da2072e185e8a4ef46d90a9f29a289d104370f6d
vadmiralov87 <vadmiralov.87@gmail.com>
2018-03-22 20:35:40 +0200

    add conf 1

--- deploy.groovy
+++ deploy.groovy
@@ -6,7 +6,7 @@ node {
 			#sudo docker-machine create --driver amazonec2 --amazonec2-region eu-central-1 --amazonec2-security-group launch-wizard-55 --amazonec2-instance-type t2.micro vadmiralov.2
            	#не забыть  поменять!!!!sudo docker-machine create --driver amazonec2 --amazonec2-region eu-west-1 --amazonec2-security-group launch-wizard-55 --amazonec2-instance-type t2.micro vadmiralov.2
            	eval $(sudo docker-machine env vadmiralov.2)
-           	sudo docker run -d -p 80:80 vadmiralov/nginx
+           	sudo docker run -d -p 80:80 vadmiralov87/nginx
            	echo "http://"`sudo docker-machine ip vadmiralov.2`
            	eval $(sudo docker-machine env -u)
 		'''
