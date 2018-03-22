699ef92e9e2f4c3eb1c777d29c92164284bc831c
vadmiralov87 <vadmiralov.87@gmail.com>
2018-03-22 19:55:59 +0200

    add conf 1

--- deploy.groovy
+++ deploy.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 node {
-	stage() {
+	stage('Deploy') {
 		sh '''
 			sudo docker-machine create --driver amazonec2 --amazonec2-region eu-central-1 --amazonec2-security-group launch-wizard-55 --amazonec2-instance-type t2.micro vadmiralov.2
            	#не забыть  поменять!!!!sudo docker-machine create --driver amazonec2 --amazonec2-region eu-west-1 --amazonec2-security-group launch-wizard-55 --amazonec2-instance-type t2.micro vadmiralov.2
