e305eb99651ae4382d74f4ef5c880a2cbe8b7f77
vadmiralov87 <vadmiralov.87@gmail.com>
2018-03-22 20:18:10 +0200

    add conf 1

--- compile_dockerize.groovy
+++ compile_dockerize.groovy
@@ -13,7 +13,7 @@ node {
                 wget http://luajit.org/download/LuaJIT-2.0.5.tar.gz
                 tar xf LuaJIT-2.0.5.tar.gz
                 rm LuaJIT-2.0.5.tar.gz
-                cd $home/LuaJIT-2.0.5 && sudo make && sudo mkdir /usr/local/include && sudo mkdir /usr/local/share/luajit-2.0.5 && sudo checkinstall -D -y --install=yes
+                cd $home/LuaJIT-2.0.5 && sudo make && sudo mkdir -p /usr/local/include && sudo mkdir -p /usr/local/share/luajit-2.0.5 && sudo checkinstall -D -y --install=yes
                 cp $home/LuaJIT-2.0.5/luajit_2.0.5-1_amd64.deb $home
                 cd $home/
             fi
@@ -34,7 +34,7 @@ node {
             
             echo "build nginx with lua-nginx-module"
             cd $home/nginx-1.13.10 && sudo ./configure --prefix=/opt/nginx --with-ld-opt="-Wl,-rpath,/usr/local/lib" --add-module=$home/ngx_devel_kit-0.3.0/ --add-module=$home/lua-nginx-module-0.10.11/ && sudo make -j2 && sudo checkinstall -D -y --install=no
-            cp $home/nginx-1.13.10/nginx_1.13.10-1_amd64.deb $home/
+            #cp $home/nginx-1.13.10/nginx_1.13.10-1_amd64.deb $home/
             cd $home
         '''
     }
